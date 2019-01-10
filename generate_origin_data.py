d8c6f7abe5d6b85f46a520da1d1feba51857a3d4
DJ <932850725@qq.com>
2019-01-10 19:29:26 +0800

    一层次分类（4分类、5分类）、二层次分类（原始详细分类）

--- generate_origin_data.py
+++ generate_origin_data.py
@@ -1,4 +1,5 @@
 import json
+###PDTB###
 dict_sense_to_label = {
     'Temporal':0,
     'Temporal.Asynchronous':1,
@@ -22,6 +23,33 @@ dict_sense_to_label = {
     'Expansion.Exception': 19,
     'EntRel': 20,
 }
+
+###PDTB四分类
+dict_sense_to_label4 = {
+    'Temporal':3,
+    'Temporal.Asynchronous':3,
+    'Temporal.Asynchronous.Precedence': 3,
+    'Temporal.Asynchronous.Succession': 3,
+    'Temporal.Synchrony': 3,
+    'Contingency':1,
+    'Contingency.Cause':1,
+    'Contingency.Cause.Reason': 1,
+    'Contingency.Cause.Result': 1,
+    'Contingency.Condition': 1,
+    'Comparison':0,
+    'Comparison.Contrast': 0,
+    'Comparison.Concession': 0,
+    'Expansion':2,
+    'Expansion.Conjunction': 2,
+    'Expansion.Instantiation': 2,
+    'Expansion.Restatement': 2,
+    'Expansion.Alternative': 2,
+    'Expansion.Alternative.Chosen alternative': 2,
+    'Expansion.Exception': 2,
+    'EntRel': 4,
+}
+
+###CDTB
 # dict_sense_to_label={
 #     'Causation':0,
 #     'Conditional':1,
@@ -35,14 +63,47 @@ dict_sense_to_label = {
 #     'Alternative':9,
 # }
 
+# 21分类：
+# ex:
+#     sentence_ex
+#     sense_ex
+#     label_ex
+# im:
+#     sentence_im
+#     sense_im
+#     label_im
+
+# 5分类(仅在第一层分类，分为0-4)
+# im:
+#     sentence_im（上）
+#     sense_im（上）
+#     label_im5
+    
+# 4分类（仅在第一层分类，不算EntRel，0-3）
+# ex:
+#     sentence_ex（上）
+#     sense_ex（上）
+#     label_ex4
+# im:
+#     sentence_im4
+#     sense_im4
+#     label_im4
+    
+
+
 cnt=1
 with open('relations_格式化.json',"r", encoding='UTF-8') as f,\
         open('sentence_ex','w', encoding='UTF-8') as fsentence_ex,\
         open('sense_ex','w', encoding='UTF-8') as fsense_ex,\
         open('label_ex','w', encoding='UTF-8') as flabel_ex,\
+        open('label_ex4','w', encoding='UTF-8') as flabel_ex4,\
         open('sentence_im','w', encoding='UTF-8') as fsentence_im,\
+        open('sentence_im4','w', encoding='UTF-8') as fsentence_im4,\
         open('sense_im','w', encoding='UTF-8') as fsense_im,\
-        open('label_im','w', encoding='UTF-8') as flabel_im:
+        open('sense_im4','w', encoding='UTF-8') as fsense_im4,\
+        open('label_im','w', encoding='UTF-8') as flabel_im,\
+        open('label_im4','w', encoding='UTF-8') as flabel_im4,\
+        open('label_im5','w', encoding='UTF-8') as flabel_im5:
     jrelation=json.load(f)
     docid=0
     for r in jrelation:
@@ -144,7 +205,18 @@ with open('relations_格式化.json',"r", encoding='UTF-8') as f,\
             fsentence_ex.write(sen+'\n')
             fsense_ex.write(sense+'\n')
             flabel_ex.write(str(dict_sense_to_label[sense])+'\n')
-        else:
+            flabel_ex4.write(str(dict_sense_to_label4[sense])+'\n')
+        elif r['Type'] == "EntRel":
             fsentence_im.write(sen+'\n')
             fsense_im.write(sense+'\n')
             flabel_im.write(str(dict_sense_to_label[sense])+'\n')
+            flabel_im5.write(str(dict_sense_to_label4[sense])+'\n')
+        else:
+            fsentence_im.write(sen+'\n')
+            fsentence_im4.write(sen+'\n')
+            fsense_im.write(sense+'\n')
+            fsense_im4.write(sense+'\n')
+            flabel_im.write(str(dict_sense_to_label[sense])+'\n')
+            flabel_im4.write(str(dict_sense_to_label4[sense])+'\n')
+            flabel_im5.write(str(dict_sense_to_label4[sense])+'\n')
+print('------------------seccess!')
